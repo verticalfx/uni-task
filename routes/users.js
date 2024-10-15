0928c062fc669fa4b177bec3017efc1238153a3a
Tommy <38462070+verticalfx@users.noreply.github.com>
2024-10-15 11:29:31 +0100

    Fix logout response link to redirect to the correct home path in the users router

--- routes/users.js
+++ routes/users.js
@@ -123,7 +123,7 @@ router.get('/logout', redirectLogin, (req, res) => {
         if (err) {
             return res.redirect('./')
         }
-        res.send('you are now logged out. <a href=' + './' + '>Home</a>');
+        res.send('you are now logged out. <a href=' + '../' + '>Home</a>');
     })
 })
 
